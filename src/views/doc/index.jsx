8e10e98152807561b2241c2baaef18f4807145c0
xiebo@asisinfo.com <xiebo@asisinfo.com>
2020-06-22 13:08:38 +0800

    visual-react-admin

--- src/views/doc/index.jsx
+++ src/views/doc/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import TypingCard from '@/components/TypingCard'
 const Doc = () => {
   const cardContent = `
-    开发文档请戳这里 <a href="https://nlrx-wjc.github.io/react-antd-admin-template-doc/" target="_blank">react-antd-admin-template开发文档</a>。
+    开发文档请戳这里 <a href="#" target="_blank">visual-react-admin开发文档</a>。
     目前正在编写完善中...
   `
   return (
